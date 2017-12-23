aea2770454156867617724f4285aa026fff1481d
liuxiaokun <liuxiaokun@corp.netease.com>
2017-12-23 19:09:26 +0800

    add python tec

--- py-algorithm/sort/quick_sort.py
+++ py-algorithm/sort/quick_sort.py
@@ -11,51 +11,51 @@
 """
 
 
-def quick_sort(numbers = []):
-	if len(numbers) <= 1:
-		return numbers
-	left_numbers = []
-	right_numbers = []
-	same = []
-	point = numbers[int(len(numbers)/2)]
-	for num in numbers:
-		if num > point:
-			right_numbers.append(num)
-		elif num < point:
-			left_numbers.append(num)
-		else:
-			same.append(num)
-	new_left = quick_sort(left_numbers)
-	new_right = quick_sort(right_numbers)
-	return new_left + same + new_right
+def quick_sort(numbers=[]):
+    if len(numbers) <= 1:
+        return numbers
+    left_numbers = []
+    right_numbers = []
+    same = []
+    point = numbers[int(len(numbers) / 2)]
+    for num in numbers:
+        if num > point:
+            right_numbers.append(num)
+        elif num < point:
+            left_numbers.append(num)
+        else:
+            same.append(num)
+    new_left = quick_sort(left_numbers)
+    new_right = quick_sort(right_numbers)
+    return new_left + same + new_right
 
 
 #  算法优化：上述算法增加了空间存储，每次递归都会创建一个列表的空间内存，现在要消除每次递归创建列表空间内存
 
 def my_quick_sort(numbers):
-	if not numbers or len(numbers) <= 1:
-		return numbers
-# 	以第一个数作为基准数
-	point = numbers[0]
-	left,right = 1,len(numbers) - 1
-	while left < right:
-# 		从右边开始遍历，直到找到比point小对应的索引
-		while numbers[right] > point and left < right:
-			right -= 1
-# 		表示从最右边找到比point小的索引
+    if not numbers or len(numbers) <= 1:
+        return numbers
+    # 	以第一个数作为基准数
+    point = numbers[0]
+    left, right = 1, len(numbers) - 1
+    while left < right:
+        # 		从右边开始遍历，直到找到比point小对应的索引
+        while numbers[right] > point and left < right:
+            right -= 1
+        # 		表示从最右边找到比point小的索引
 
-# 		开始从最左边查找比point大的索引
-		while numbers[left] < point and left < right:
-			left += 1
-# 		表示从最左边（除了point的索引）找到比point小的索引
+        # 		开始从最左边查找比point大的索引
+        while numbers[left] < point and left < right:
+            left += 1
+        # 		表示从最左边（除了point的索引）找到比point小的索引
 
-		if left < right:
-			numbers[left],numbers[right] = numbers[right],numbers[left]
-	numbers[0],numbers[left] = numbers[left],numbers[0]
-	return my_quick_sort(numbers[0:left]) + [point] + my_quick_sort(numbers[left+1:])
+        if left < right:
+            numbers[left], numbers[right] = numbers[right], numbers[left]
+    numbers[0], numbers[left] = numbers[left], numbers[0]
+    return my_quick_sort(numbers[0:left]) + [point] + my_quick_sort(
+        numbers[left + 1:])
 
 
-numbers = [12,3,7,9,6,23,10,11,24,25,17,4,8,19]
+numbers = [12, 3, 7, 9, 6, 23, 10, 11, 24, 25, 17, 4, 8, 19]
 new_number = my_quick_sort(numbers)
 print(new_number)
-
--- py-basic/py-core/types/numbers/number.py
+++ py-basic/py-core/types/numbers/number.py
@@ -2,12 +2,170 @@
 # -*- coding: UTF-8 -*-
 
 """
-python 数字类型
-1. 数字类型基础
-2. 数字类型实践
-3. 其他的数字类型
-4. 数字类型扩展
+## Integers (unlimited size)
+1234 ,  −24 ,  0 ,  99999999999999 
+
+## Floating-point numbers
+1.23 ,  1. ,  3.14e-10 ,  4E210 ,  4.0e+210 
+
+## Octal, hex, and binary literals in 3.X
+0o177 ,  0x9ff ,  0b101010 
+
+## Octal, octal, hex, and binary literals in 2.X
+0177 ,  0o177 ,  0x9ff ,  0b101010 
+
+## Complex number literals
+3+4j ,  3.0+4.0j ,  3J
+
+## Sets: 2.X and 3.X construction forms
+set('spam'), {1, 2, 3, 4} 
+
+## Decimal and fraction extension types
+Decimal('1.0'), Fraction(1, 3)
+
+##  Boolean type and constants
+bool(X), True, False
+
+## Expression operators
++ ,  - ,  * ,  / ,  >> ,  ** ,  & , etc.
+
+## Built-in mathematical functions
+pow ,  abs ,  round ,  int ,  hex ,  bin , etc.
+
+## Utility modules
+random ,  math
 """
+
+"""
+## Generator function  send protocol
+yield x 
+
+## Anonymous function generation
+lambda args: expression 
+
+## Ternary selection ( x is evaluated only if  y is true)
+x if y else z 
+
+## Logical OR ( y is evaluated only if  x is false)
+x or y 
+
+## Logical AND ( y is evaluated only if  x is true)
+x and y
+
+## Logical negation 
+not x 
+
+## x not in y Membership (iterables, sets)
+x in y 
+
+##  x is not y Object identity tests
+x is y , 
+
+## Magnitude comparison, set subset and superset;
+x < y ,  x <= y ,  x > y ,  x >= y
+
+## Value equality operators
+x == y ,  x != y
+
+## Bitwise OR, set union
+x | y
+
+##  Bitwise XOR, set symmetric difference 
+x ^ y
+
+## Bitwise AND, set intersection
+x & y 
+
+## Shift  x left or right by  y bits
+x << y ,  x >> y 
+
+## Addition, concatenation;
+x + y
+
+## Subtraction, set difference
+x – y
+
+## Multiplication, repetition;
+x * y
+
+## Remainder, format;
+x % y
+
+## Division: true and floor
+x / y ,  x // y
+
+## Negation, identity
+−x ,  +x 
+
+## Bitwise NOT (inversion)
+˜x 
+
+## Power (exponentiation)
+x ** y 
+
+## Indexing (sequence, mapping, others)
+x[i] 
+
+## Slicing
+x[i:j:k] 
+
+##  Call (function, method, class, other callable)
+x(...)
+
+## Attribute reference
+x.attr
+
+## Tuple, expression, generator expression 
+(...) 
+
+## List, list comprehension
+[...] 
+
+## Dictionary, set, set and dictionary comprehensions
+{...} 
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
